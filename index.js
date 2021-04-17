383e9f719e5c891f612e9e4fbd9dc3cc4c021c8c
kgthree-kgthree <77081399+kgthree-kgthree@users.noreply.github.com>
2021-04-17 02:12:54 +0200

    Add files via upload
    Virtual Drums!!- created as part of Web Development Bootcamp

---
+++ index.js
@@ -0,0 +1,62 @@
+// Detect Clicks
+
+var drumButtonObject = document.querySelectorAll(".drum");
+for (var i = 0; i < drumButtonObject.length; i++) {
+  drumButtonObject[i].addEventListener("click", function() {
+    playSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
+}
+
+// Detect Keyboard press
+
+  document.addEventListener("keydown", function() {
+    // console.log(event.key);
+    playSound(event.key);
+    buttonAnimation(event.key);
+  });
+
+
+  // this.style.color = "white";
+
+  function playSound(keySelected) {
+    switch (keySelected) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      default:
+        console.log(keySelected);
+    }
+  }
+
+function buttonAnimation(selectedKey) {
+  makeActive = "."+selectedKey;
+  console.log(makeActive);
+  document.querySelector(makeActive).classList.add("pressed");
+  window.setInterval(function() {document.querySelector(makeActive).classList.remove("pressed");}, 500);
+}
